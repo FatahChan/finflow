af5928d6c2be1368124207f178ea48c152b2b78e
FatahChan <ahmadfathallah89@gmail.com>
2025-03-23 16:13:28 +0200

    fix: lint

--- src/lib/database.types.ts
+++ src/lib/database.types.ts
@@ -4,150 +4,150 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[]
+  | Json[];
 
 export type Database = {
   graphql_public: {
     Tables: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
       graphql: {
         Args: {
-          operationName?: string
-          query?: string
-          variables?: Json
-          extensions?: Json
-        }
-        Returns: Json
-      }
-    }
+          operationName?: string;
+          query?: string;
+          variables?: Json;
+          extensions?: Json;
+        };
+        Returns: Json;
+      };
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+      [_ in never]: never;
+    };
+  };
   public: {
     Tables: {
       account: {
         Row: {
-          created_at: string
-          currency_code: string
-          deleted: boolean
-          id: string
-          name: string
-          updated_at: string
-          user_id: string
-        }
+          created_at: string;
+          currency_code: string;
+          deleted: boolean;
+          id: string;
+          name: string;
+          updated_at: string;
+          user_id: string;
+        };
         Insert: {
-          created_at?: string
-          currency_code: string
-          deleted?: boolean
-          id?: string
-          name: string
-          updated_at?: string
-          user_id?: string
-        }
+          created_at?: string;
+          currency_code: string;
+          deleted?: boolean;
+          id?: string;
+          name: string;
+          updated_at?: string;
+          user_id?: string;
+        };
         Update: {
-          created_at?: string
-          currency_code?: string
-          deleted?: boolean
-          id?: string
-          name?: string
-          updated_at?: string
-          user_id?: string
-        }
+          created_at?: string;
+          currency_code?: string;
+          deleted?: boolean;
+          id?: string;
+          name?: string;
+          updated_at?: string;
+          user_id?: string;
+        };
         Relationships: [
           {
-            foreignKeyName: "account_currency_code_fkey"
-            columns: ["currency_code"]
-            isOneToOne: false
-            referencedRelation: "currency"
-            referencedColumns: ["code"]
+            foreignKeyName: "account_currency_code_fkey";
+            columns: ["currency_code"];
+            isOneToOne: false;
+            referencedRelation: "currency";
+            referencedColumns: ["code"];
           },
-        ]
-      }
+        ];
+      };
       currency: {
         Row: {
-          code: string
-          slug: string
-          symbol: string
-        }
+          code: string;
+          slug: string;
+          symbol: string;
+        };
         Insert: {
-          code: string
-          slug: string
-          symbol: string
-        }
+          code: string;
+          slug: string;
+          symbol: string;
+        };
         Update: {
-          code?: string
-          slug?: string
-          symbol?: string
-        }
-        Relationships: []
-      }
+          code?: string;
+          slug?: string;
+          symbol?: string;
+        };
+        Relationships: [];
+      };
       transaction: {
         Row: {
-          account_id: string
-          amount: number
-          created_at: string
-          deleted: boolean
-          id: string
-          name: string
-          transaction_type: Database["public"]["Enums"]["transaction type"]
-          updated_at: string
-        }
+          account_id: string;
+          amount: number;
+          created_at: string;
+          deleted: boolean;
+          id: string;
+          name: string;
+          transaction_type: Database["public"]["Enums"]["transaction type"];
+          updated_at: string;
+        };
         Insert: {
-          account_id: string
-          amount: number
-          created_at?: string
-          deleted?: boolean
-          id?: string
-          name: string
-          transaction_type?: Database["public"]["Enums"]["transaction type"]
-          updated_at?: string
-        }
+          account_id: string;
+          amount: number;
+          created_at?: string;
+          deleted?: boolean;
+          id?: string;
+          name: string;
+          transaction_type?: Database["public"]["Enums"]["transaction type"];
+          updated_at?: string;
+        };
         Update: {
-          account_id?: string
-          amount?: number
-          created_at?: string
-          deleted?: boolean
-          id?: string
-          name?: string
-          transaction_type?: Database["public"]["Enums"]["transaction type"]
-          updated_at?: string
-        }
+          account_id?: string;
+          amount?: number;
+          created_at?: string;
+          deleted?: boolean;
+          id?: string;
+          name?: string;
+          transaction_type?: Database["public"]["Enums"]["transaction type"];
+          updated_at?: string;
+        };
         Relationships: [
           {
-            foreignKeyName: "transactions_account_id_fkey"
-            columns: ["account_id"]
-            isOneToOne: false
-            referencedRelation: "account"
-            referencedColumns: ["id"]
+            foreignKeyName: "transactions_account_id_fkey";
+            columns: ["account_id"];
+            isOneToOne: false;
+            referencedRelation: "account";
+            referencedColumns: ["id"];
           },
-        ]
-      }
-    }
+        ];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      "transaction type": "debit" | "credit"
-    }
+      "transaction type": "debit" | "credit";
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
+      [_ in never]: never;
+    };
+  };
+};
 
-type PublicSchema = Database[Extract<keyof Database, "public">]
+type PublicSchema = Database[Extract<keyof Database, "public">];
 
 export type Tables<
   PublicTableNameOrOptions extends
@@ -160,7 +160,7 @@ export type Tables<
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
+      Row: infer R;
     }
     ? R
     : never
@@ -168,11 +168,11 @@ export type Tables<
         PublicSchema["Views"])
     ? (PublicSchema["Tables"] &
         PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-        Row: infer R
+        Row: infer R;
       }
       ? R
       : never
-    : never
+    : never;
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
@@ -183,17 +183,17 @@ export type TablesInsert<
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
+      Insert: infer I;
     }
     ? I
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I
+        Insert: infer I;
       }
       ? I
       : never
-    : never
+    : never;
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
@@ -204,17 +204,17 @@ export type TablesUpdate<
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
+      Update: infer U;
     }
     ? U
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U
+        Update: infer U;
       }
       ? U
       : never
-    : never
+    : never;
 
 export type Enums<
   PublicEnumNameOrOptions extends
@@ -227,14 +227,14 @@ export type Enums<
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
     ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-    : never
+    : never;
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
     | keyof PublicSchema["CompositeTypes"]
     | { schema: keyof Database },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof Database
+    schema: keyof Database;
   }
     ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
     : never = never,
@@ -242,5 +242,4 @@ export type CompositeTypes<
   ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
   : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
     ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never
-
+    : never;
