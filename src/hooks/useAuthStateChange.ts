79866ad194747035720be61f76745b146e2192e2
FatahChan <ahmadfathallah89@gmail.com>
2025-03-29 12:31:28 +0200

    fix: sub

--- src/hooks/useAuthStateChange.ts
+++ src/hooks/useAuthStateChange.ts
@@ -1,31 +1,21 @@
 import { sessionStore$ } from "@/lib/SupaLegend";
 import { supabaseClient } from "@/lib/supabase";
-import type { Session } from "@supabase/supabase-js";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
-// const EVENT_TO_LISTEN_TO = [
-//   "SIGNED_IN",
-//   "INITIAL_SESSION",
-//   "TOKEN_REFRESHED",
-//   "SIGNED_OUT",
-// ];
 function useAuthStateChange() {
-  const [session, setSession] = useState<Session | null>(null);
   useEffect(() => {
     supabaseClient.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
+      sessionStore$.set(session);
     });
     const {
       data: { subscription },
     } = supabaseClient.auth.onAuthStateChange((_, session) => {
-      setSession(session);
       sessionStore$.set(session);
     });
     return () => {
       subscription?.unsubscribe();
     };
   }, []);
-  console.log(session);
 }
 
 export default useAuthStateChange;
