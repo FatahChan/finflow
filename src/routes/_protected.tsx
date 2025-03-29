95a67ba2b1b54f3d1c036e1a2ce944b6aeb0f3c6
FatahChan <ahmadfathallah89@gmail.com>
2025-03-29 04:42:37 +0200

    test: remove protection

--- src/routes/_protected.tsx
+++ src/routes/_protected.tsx
@@ -1,16 +1,6 @@
-import { Outlet, createFileRoute, redirect } from "@tanstack/react-router";
+import { Outlet, createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_protected")({
-  loader: async ({ context }) => {
-    return await context.supabaseClient.auth.getSession().then((res) => {
-      if (!res.data.session) {
-        throw redirect({
-          to: "/login",
-          replace: true,
-        });
-      }
-    });
-  },
   component: RouteComponent,
 });
 
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,17 +1,6 @@
 import LoginPage from "@/components/login";
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/login")({
   component: LoginPage,
-  beforeLoad: ({ context }) => {
-    context.supabaseClient.auth.getSession().then((res) => {
-      console.log(res);
-      if (res.data.session) {
-        throw redirect({
-          to: "/account",
-          replace: true,
-        });
-      }
-    });
-  },
 });
